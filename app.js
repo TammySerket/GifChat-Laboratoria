e2ca4a92697539bdb28da2957d991048a5624f46
Tamara Hinojosa Salinas <tamara.hi.sa@hotmail.com>
2018-07-10 16:13:57 -0400

    Se realizan cambios para linkear con facebook

--- app.js
+++ app.js
@@ -1,67 +1,70 @@
 window.onload = ()=>{
-    firebase.auth().onAuthStateChange((user)=>{
-        if(user){
+    firebase.auth().onAuthStateChanged((user)=>{
+        if(user){ //Si está logeado, mostraremos la opción loggedIn
             loggedIn.style.display = "block";
             loggedOut.style.display = "none";
-        }else{
+            username.innerText = user.displayName;
+        }else{ //Si NO está logeado, mostraremos la opción loggedOut
             loggedIn.style.display = "none";
             loggedOut.style.display = "block";
         }
-        console.log("User >"+JSON.stringify(user));
+        console.log("User > "+JSON.stringify(user));
     });
 }
 
+//Registro
 function registerWithFirebase(){
-  const emailValue = email.value;
-  const passwordValue = password.value;
+    const emailValue = email.value;
+    const passwordValue = password.value;
 
-  firebase.auth().createUserWithEmailAndPassword(emailValue, passwordValue)
-  .then(()=>{
-     console.log("usuario creado con exito")
-  })
-  .catch((error)=>{
-     console.log("Error de Firebase > Código >"+error.code);
-     console.log("Error de Firebase > Mensaje >"+error.message);
-  })
+    firebase.auth().createUserWithEmailAndPassword(emailValue, passwordValue)
+        .then(()=>{
+            console.log("Usuario creado con éxito");
+        })
+        .catch((error)=>{
+            console.log("Error de firebase > Código > "+error.code); //error.code nos mostrará el código de error para informarnos qué pasó
+            console.log("Error de firebase > Mensaje > "+error.message); //error.message nos mostrará el mensaje de firebase del mismo error
+        });
 }
-
+//Login
 function loginWithFirebase(){
     const emailValue = email.value;
     const passwordValue = password.value;
 
-    firebase.auth().singInWithEmailAndPassword(emailValue, passwordValue)
-    .then(()=>{
-      console.log("Usuario inicio sesion con exito")
-    })
-    .catch((error)=>{
-        console.log("Error de Firebase > Código >"+error.code);
-        console.log("Error de Firebase > Mensaje >"+error.message);
-    })
+    firebase.auth().signInWithEmailAndPassword(emailValue, passwordValue)
+        .then(()=>{
+            console.log("Usuario inició sesión con éxito");
+        })
+        .catch((error)=>{
+            console.log("Error de firebase > Código > "+error.code); //error.code nos mostrará el código de error para informarnos qué pasó
+            console.log("Error de firebase > Mensaje > "+error.message); //error.message nos mostrará el mensaje de firebase del mismo error
+        });
 }
 
 function logoutWithFirebase(){
-    firebase.auth().singOut()
-    .then(()=>{
-        console.log("usuario finalizó sesion con exito")
-    })
-    .catch((error)=>{
-        console.log("Error de Firebase > Código >"+error.code);
-        console.log("Error de Firebase > Mensaje >"+error.message);
-    })
+    firebase.auth().signOut()
+        .then(()=>{
+            console.log("Usuario finalizó su sesión");
+        })
+        .catch((error)=>{
+            console.log("Error de firebase > Código > "+error.code); //error.code nos mostrará el código de error para informarnos qué pasó
+            console.log("Error de firebase > Mensaje > "+error.message); //error.message nos mostrará el mensaje de firebase del mismo error
+        });
 }
-
+//Login con Facebook
 function facebookLoginWithFirebase(){
-    const provider = new firebase.auth.facbookAuthProvider();
+    const provider = new firebase.auth.FacebookAuthProvider(); // creamos un nuevo objeto 
 
-    provider.setCustomParameters({
-        "display" : "popup"
-    })
+    provider.setCustomParameters({ // le decimos que haga un login con facebook y enlace un popup
+        'display' : 'popup'
+    });
+
     firebase.auth().signInWithPopup(provider)
-    .then(()=>{
-        console.log("Login con Facebook exitoso")
-    })
-    .catch((error)=>{
-        console.log("Error de Firebase > Código >"+error.code);
-        console.log("Error de Firebase > Mensaje >"+error.message);
-    })
+        .then(()=>{
+            console.log("Login con facebook exitoso");
+        })
+        .catch((error)=>{
+            console.log("Error de firebase > Código > "+error.code); //error.code nos mostrará el código de error para informarnos qué pasó
+            console.log("Error de firebase > Mensaje > "+error.message); //error.message nos mostrará el mensaje de firebase del mismo error
+        });                        
 }
